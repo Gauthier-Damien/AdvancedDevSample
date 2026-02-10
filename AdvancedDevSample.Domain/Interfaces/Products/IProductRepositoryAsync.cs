c8667d51696366a0d7a6d128d8412ad587a3d02f
Nieraco <damsgauth@gmail.com>
2026-02-10 11:14:33 +0100

    fix: ajout ProducesResponseType avec types de retour sur OrderController + correction IProductRepositoryAsync

--- AdvancedDevSample.Domain/Interfaces/Products/IProductRepositoryAsync.cs
+++ AdvancedDevSample.Domain/Interfaces/Products/IProductRepositoryAsync.cs
@@ -1,6 +1,13 @@
+using AdvancedDevSample.Domain.Entities;
+
 namespace AdvancedDevSample.Domain.Interfaces.Products;
 
-public class IProductRepositoryAsync
+/// <summary>
+/// Port (Interface) : Contrat de persistance asynchrone des produits
+/// Utilisé pour les tests d'intégration et les opérations async
+/// </summary>
+public interface IProductRepositoryAsync
 {
-    
+    Task<Product?> GetByIdAsync(Guid id);
+    Task SaveAsync(Product product);
 }
